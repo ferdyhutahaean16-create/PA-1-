382637f6bff6b62d23ad696b7fd4a140f34ac4f4
ferdyhutahaean16-create <ferdyhutahaean16@gmail.com>
2026-02-11 09:23:24 +0700

    Upload pertama project Laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main', ['activePage' => 'home', 'titlePage' => 'Home'])
+
+@section('title', 'Home')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12">
+            <h3>Hello Content</h3>
+        </div>
+    </div>
+@endsection
